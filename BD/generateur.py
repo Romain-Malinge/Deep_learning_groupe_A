bde65d29e605ec2ca6fe695247f1a384ef79c41c
Romain-Malinge <romain.malinge.pro@gmail.com>
2024-03-27 18:26:58 +0100

    Avant gros push

--- BD/generateur.py
+++ BD/generateur.py
@@ -10,8 +10,9 @@ import os
 import shutil
 import math
 
-NOMBRE_IMAGE = 4000                         # Nombre d'image à générer
-NOMBRE_DE_CLASSE = 4                        # Le nombre de pokemon max par image - 1
+DIR_NAME = 'validation'                     # Nom du dossier pour les images
+NOMBRE_IMAGE = 1000                         # Nombre d'image à générer
+NOMBRE_DE_CLASSE = 4                        # Nombre de pokemon max par image
 NOMBRE_POKEMON_TOTAL = 649                  # Nombre de pokemon possible 
 NOMBRE_BACKGROUND_TOTAL = 30                # Nombre de fond possible
 L = 256                                     # Largueur background
@@ -19,15 +20,18 @@ H = 192                                     # Hauteur background
 l = 96                                      # largueur image pokemon
 h = 96                                      # hauteur image pokemon
 DEBORDEMENT = math.floor(h/3)               # Debordement sur les bords de l'image en pixel
-DISTANCE_MIN = math.floor(h/2)              # Distance min entre 2 pokemons
+DISTANCE_MIN = math.floor(h/1.5)            # Distance min entre 2 pokemons
 SCALE_COEF = 1.5                            # Valriation max de la taille
-any = (-2*DISTANCE_MIN, -2*DISTANCE_MIN)    # Position eloigné de tout 
+PERCENT_DARK = 20                           # Pourcentage de fond sombre
+PERCENT_ELEM = 50                           # Pourcentage d'image avec des éléments
+NOMBRE_ELEM = 5                             # Nombre d'éléments différents
 
+any = (-2*DISTANCE_MIN, -2*DISTANCE_MIN)    # Position eloigné de tout
 
 def generate_images():
 
     # Supprimer tous les dossiers existants
-    path = 'entrainement'
+    path = DIR_NAME
     if os.path.exists(path):
         shutil.rmtree(path)
     else:
@@ -43,7 +47,9 @@ def generate_images():
 
         # Ouvrir le background
         num_background = random.randint(1, NOMBRE_BACKGROUND_TOTAL)
-        background = Image.open('backgrounds\\' + str(num_background) + '.png')
+        image = Image.open('data\\backgrounds\\' + str(num_background) + '.png')
+        if random.randint(1, 100) <= PERCENT_DARK:
+            image = image.point(adjust_brightness)
 
         # Ajouter les pokemons
         nombre_pokemon = random.randint(1, NOMBRE_DE_CLASSE)
@@ -56,10 +62,17 @@ def generate_images():
             while pos_not_ok(centre, mem):
                 (pos, centre) = new_position(pokemon)
             mem.append(centre)
-            background.paste(pokemon, pos, pokemon)
+            image.paste(pokemon, pos, pokemon)
+
+        # Ajouter un élément
+        if random.randint(1, 100) <= PERCENT_ELEM:
+            for _ in range(2):
+                num_image = random.randint(1, NOMBRE_ELEM)
+                elem = Image.open(os.path.join('data\\elements', f'{num_image}.png'))
+                image.paste(elem, (0,0), elem)
 
         # Enregistrer l'image
-        background.save(path + '\\' + str(nombre_pokemon) + '\\' + str(i) + '.png')
+        image.save(path + '\\' + str(nombre_pokemon) + '\\' + str(i) + '.png')
 
 
 # Retourne une image de pokémon au hasard
@@ -67,7 +80,7 @@ def new_pokemon():
     # Tirer le numéro et le coté du pokémon
     num_pokemon = random.randint(1, NOMBRE_POKEMON_TOTAL)
     cote_pokemon = 'front' if random.randint(0, 1) else 'back'
-    pokemon = Image.open(os.path.join('pokemons', cote_pokemon, f'{num_pokemon}.png'))
+    pokemon = Image.open(os.path.join('data\\pokemons', cote_pokemon, f'{num_pokemon}.png'))
     pokemon = pokemon.convert("RGBA")
     # Changer la taille du pokémon
     nouvelle_taille = random.randint(h, math.floor(h*SCALE_COEF))
@@ -95,6 +108,10 @@ def pos_not_ok(centre, mem):
         if distance(centre, mem[i]) < DISTANCE_MIN:
             return True
     return False
+
+# Réduit la luminosité à 50%
+def adjust_brightness(pixel_value):
+    return int(pixel_value * 0.5)
 
 
 generate_images()
